282cfa2f172ba54063aba69664c841ff0d1e136b
GabrielF-S <gabriel.fsantos97@outlook.com>
2025-05-13 10:54:50 -0300

    :tada: init: Initial commit

---
+++ src/main/java/com/doces/beersheva/BeershevaApplication.java
@@ -0,0 +1,13 @@
+package com.doces.beersheva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeershevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeershevaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doces/beersheva/BeershevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doces.beersheva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeershevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
